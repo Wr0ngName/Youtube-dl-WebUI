d0674a98457ea9414f625e312dab253a9ede484e
Florent <florent73@laposte.net>
2021-05-30 18:47:05 +0200

    Securing download page

--- get.php
+++ get.php
@@ -5,36 +5,43 @@
 
     if(isset($_GET['logout']) && $_GET['logout'] == 1) endSession();
 
-    if(isset($_GET['fileToGet']))
+    if(isset($_SESSION['logged']) && $_SESSION['logged'] == 1)
     {
-        $rawFile = htmlspecialchars($_GET['fileToGet']);
-        $tmpFile = pathinfo($rawFile);
-        $fileToGet  = $tmpFile['basename'];
-
-        if(file_exists($folder.$fileToGet) && substr($fileToGet, 0, 1) != '/' && substr($fileToGet, 0, 1) != '.')
+        if(isset($_GET['fileToGet']))
         {
-            //Get file type and set it as Content Type
-            $finfo = finfo_open(FILEINFO_MIME_TYPE);
-            header('Content-Type: ' . finfo_file($finfo, $folder.$fileToGet));
-            finfo_close($finfo);
+            $rawFile = htmlspecialchars($_GET['fileToGet']);
+            $tmpFile = pathinfo($rawFile);
+            $fileToGet  = $tmpFile['basename'];
 
-            //Use Content-Disposition: attachment to specify the filename
-            header('Content-Disposition: attachment; filename='.basename($folder.$fileToGet));
+            if(file_exists($folder.$fileToGet) && substr($fileToGet, 0, 1) != '/' && substr($fileToGet, 0, 1) != '.')
+            {
+                //Get file type and set it as Content Type
+                $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                header('Content-Type: ' . finfo_file($finfo, $folder.$fileToGet));
+                finfo_close($finfo);
 
-            //No cache
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
+                //Use Content-Disposition: attachment to specify the filename
+                header('Content-Disposition: attachment; filename='.basename($folder.$fileToGet));
 
-            //Define file size
-            header('Content-Length: ' . filesize($folder.$fileToGet));
+                //No cache
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
 
-            ob_clean();
-            flush();
-            readfile($folder.$fileToGet);
-            exit;
+                //Define file size
+                header('Content-Length: ' . filesize($folder.$fileToGet));
 
+                ob_clean();
+                flush();
+                readfile($folder.$fileToGet);
+                exit;
+
+            }
         }
+    }
+    else
+    {
+        header("Location: /".$listPage);
     }
 
 ?>
